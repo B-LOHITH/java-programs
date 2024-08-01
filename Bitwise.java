34452a8055ba157a7170e6ca31ca506921d84d93
B-LOHITH <23PA1A0532@vishnu.edu.in>
2024-08-01 16:06:38 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Bitwise
+{
+  public static void main(String[]args)
+  {
+     int a;
+     int b;
+     Scanner sc= new Scanner(System.in);
+     System.out.println("Enter a, b values");
+     a=sc.nextInt();
+     b=sc.nextInt(); 
+     
+     System.out.println(a|b);
+     System.out.println(a&b);
+     System.out.println();
+     }
+     }
